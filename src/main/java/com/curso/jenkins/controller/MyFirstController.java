9a04bf5d3fe019f6d379d603eec5fdfa2f952b91
alvaro-gimenez-gorris <97445361+alvaro-gimenez-gorris@users.noreply.github.com>
2023-03-20 12:18:05 +0100

    Update MyFirstController.java

--- src/main/java/com/curso/jenkins/controller/MyFirstController.java
+++ src/main/java/com/curso/jenkins/controller/MyFirstController.java
@@ -8,6 +8,6 @@ public class MyFirstController {
 
     @GetMapping("/hola")
     public String controller(){
-        return "Hola desde el servidor de producción actualizado";
+        return "Hola desde el servidor de producción actualizado!!";
     }
 }
